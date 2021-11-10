312dcbb33873748b051d6b7f3bd5bd85d6d13c0a
Dragon-KK <61528852+Dragon-KK@users.noreply.github.com>
2021-11-10 20:58:08 +0530

    started progress again :(

--- front/app/pages/engine/physics.py
+++ front/app/pages/engine/physics.py
@@ -5,3 +5,41 @@ class CollisionData:
         self.collider = collider
         self.collisionPoint = collisionPoint
         self.collisionAxis = collisionAxis
+
+class world:
+    def __init__(self, balls, walls):
+        self.balls = balls
+        self.walls = walls
+        
+
+    def calculateNextCollidingWall(self,ball):
+        potentialWalls = {}
+        for i in ball.walls:
+            absInfo = self.walls[i].getAbsoluteInfo()
+            if self.walls[i].vertical:
+                tmp = ball.direction.x * (-ball.position.x + absInfo['p1'].x)
+                if tmp > 0:
+                    potentialWalls[tmp] = self.walls[i]
+            else:
+                tmp = ball.direction.y * (-ball.position.y + absInfo['p2'].y)
+                if tmp > 0:
+                    potentialWalls[tmp] = self.walls[i]
+        if len(potentialWalls.values()) == 0:
+            # Maybe raise some error here ?
+            pass
+        else:
+            ball.nextCollidingWall = potentialWalls[min(potentialWalls.keys())]
+
+    def handleBall(self,ball):
+        for ball in self.balls:
+            ball.update()
+            self.calculateNextCollidingWall(ball)
+            print(ball.nextCollidingWall.p1, ball.nextCollidingWall.p2) if ball.nextCollidingWall else print("Not bounded")
+        # options :
+        # do collision check each frame
+        # after each collision calculate the next colliding wall and when the wall is passed handle collision and repeat !!! Do this
+        pass
+
+    def work(self, dt = 0.1):
+        for ball in self.balls:
+            self.handleBall(ball)

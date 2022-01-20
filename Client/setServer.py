3ba86a803376f3ca8dcb67b46fffc1c23750192f
Dragon-KK <61528852+Dragon-KK@users.noreply.github.com>
2022-01-20 22:12:56 +0530

    updatedgitignore

--- Client/setServer.py
+++ Client/setServer.py
@@ -1,8 +1,8 @@
 
 from App.Core.DataManagers import FileManager
 
-x = FileManager.ReadJson("Data/storage.json")
+x = FileManager.ReadJson("Data/Storage.json")
 sa = input("Enter server address : ").split(":")
 x['OnlineMultiplayer']['ServerAddress'] = [sa[0], int(sa[1])]
 
-FileManager.WriteJson("Data/storage.json", x)
+FileManager.WriteJson("Data/Storage.json", x)
